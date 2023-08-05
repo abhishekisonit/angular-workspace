2e4079815f12c180861ea6dfb5e09b0165be9b50
Abhishek Yadav <abhishekisonit@gmail.com>
2023-08-05 23:05:08 +0530

    logger component added and running

---
+++ projects/assignment-3/src/app/logger/logger.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logger',
+  templateUrl: './logger.component.html',
+})
+export class LoggerComponent {
+  title = 'app-logger';
+}
